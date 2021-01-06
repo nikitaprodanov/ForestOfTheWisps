bd846aee43ddfc932182d8cfd35a33b35dfe8c98
uchihanaruto9371 <uchihanaruto9371@gmail.com>
2021-01-06 14:47:51 +0200

    Base of dungeon generator algorithm

--- Assets/Scripts/RoomSpawner.cs
+++ Assets/Scripts/RoomSpawner.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class RoomSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int openingDirection;
+    // 1 --> need bottom door
+    // 2 --> need top door
+    // 3 --> need left door
+    // 4 --> need right door
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (openingDirection == 1)
+        {
+            // Need to spawn a room with BOTTOM door
+        } else if (openingDirection == 2)
+        {
+            // Need to spawn a room with TOP door
+        } else if (openingDirection == 3)
+        {
+            // Need to spawn a room with LEFT door
+        } else if (openingDirection == 4)
+        {
+            // Need to spawn a room with RIGHT door
+        }
     }
 }

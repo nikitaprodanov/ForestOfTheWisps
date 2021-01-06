39f95504decd8af0744642f74347cea2a1051cab
uchihanaruto9371 <uchihanaruto9371@gmail.com>
2021-01-06 16:21:18 +0200

    Dungeon generator algorithm phase one

--- Assets/Scripts/RoomSpawner.cs
+++ Assets/Scripts/RoomSpawner.cs
@@ -10,20 +10,51 @@ public class RoomSpawner : MonoBehaviour
     // 3 --> need left door
     // 4 --> need right door
 
-    private void Update()
+    private RoomTemplates templates;
+    private int rand;
+    private bool spawned = false;
+
+    private void Start()
     {
-        if (openingDirection == 1)
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn", 0.1f);
+    }
+
+    void Spawn()
+    {
+        if (spawned == false)
         {
-            // Need to spawn a room with BOTTOM door
-        } else if (openingDirection == 2)
+            if (openingDirection == 1)
+            {
+                // Need to spawn a room with BOTTOM door
+                rand = Random.Range(0, templates.bottomRooms.Length);
+                Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
+            } else if (openingDirection == 2)
+            {
+                // Need to spawn a room with TOP door
+                rand = Random.Range(0, templates.topRooms.Length);
+                Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
+            } else if (openingDirection == 3)
+            {
+                // Need to spawn a room with LEFT door
+                rand = Random.Range(0, templates.leftRooms.Length);
+                Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
+            } else if (openingDirection == 4)
+            {
+                // Need to spawn a room with RIGHT door
+                rand = Random.Range(0, templates.rightRooms.Length);
+                Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+            }
+
+            spawned = true;
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("SpawnPoint") && collision.GetComponent<RoomSpawner>().spawned == true)
         {
-            // Need to spawn a room with TOP door
-        } else if (openingDirection == 3)
-        {
-            // Need to spawn a room with LEFT door
-        } else if (openingDirection == 4)
-        {
-            // Need to spawn a room with RIGHT door
+            Destroy(gameObject);
         }
     }
 }

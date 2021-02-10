15467ba3cc56545c359f49f8dd4a15c8edb0338c
uchihanaruto9371 <uchihanaruto9371@gmail.com>
2021-02-10 18:27:07 +0200

    Player with movement keyboard

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    //public Joystick joystick;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+        //movement.x = joystick.Horizontal;
+        //movement.y = joystick.Vertical;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+}

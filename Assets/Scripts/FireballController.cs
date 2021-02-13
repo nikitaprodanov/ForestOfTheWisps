02f3a3c944c6b6cce1a74079b0a737dde3275bb8
uchihanaruto9371 <uchihanaruto9371@gmail.com>
2021-02-13 18:04:32 +0200

    Projectile optimization

---
+++ Assets/Scripts/FireballController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class FireballController : MonoBehaviour
+{
+    public float lifetime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DestroyProjectile", lifetime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+}

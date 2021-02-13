335aef8cfc119962d8ec0ee68fe2b9810adb81b9
uchihanaruto9371 <uchihanaruto9371@gmail.com>
2021-02-13 18:24:56 +0200

    Raycast enemy detection

--- Assets/Scripts/FireballController.cs
+++ Assets/Scripts/FireballController.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 public class FireballController : MonoBehaviour
 {
     public float lifetime;
+    public float distance;
+    public LayerMask whatIsSolid;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +18,15 @@ public class FireballController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.up, distance, whatIsSolid);
+        if (hitInfo.collider != null)
+        {
+            if (hitInfo.collider.CompareTag("Enemy"))
+            {
+                Debug.Log("ENEMY MUST TAKE DAMAGE");
+            }
+            DestroyProjectile();
+        }
     }
 
     void DestroyProjectile()
